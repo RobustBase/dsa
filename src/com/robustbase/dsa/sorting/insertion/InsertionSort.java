7ac5206fbb97c10060df9b73491177c7388f01cf
Atul Dwivedi <atul16dwivedi@gmail.com>
2021-07-01 21:16:20 +0530

    added insertion and selection sort algorithms

--- src/com/robustbase/dsa/sorting/insertion/InsertionSort.java
+++ src/com/robustbase/dsa/sorting/insertion/InsertionSort.java
@@ -1,8 +1,35 @@
 package com.robustbase.dsa.sorting.insertion;
 
+import java.util.Arrays;
+
 /**
  * @author Atul Dwivedi
  * @date 01/07/21
  */
 public class InsertionSort {
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        //input array
+        int[] arr = {4, 2, 9, 7, 3, 5};
+
+        //sorting
+        new InsertionSort().sort(arr);
+
+        //printing sorted array
+        Arrays.stream(arr).forEach(element -> System.out.print(element + "\t"));
+    }
 }
--- src/com/robustbase/dsa/sorting/selection/SelectionSort.java
+++ src/com/robustbase/dsa/sorting/selection/SelectionSort.java
@@ -1,8 +1,35 @@
 package com.robustbase.dsa.sorting.selection;
 
+import java.util.Arrays;
+
 /**
  * @author Atul Dwivedi
  * @date 01/07/21
  */
 public class SelectionSort {
+
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        //input array
+        int[] arr = {4, 2, 9, 7, 3, 5};
+
+        //sorting
+        new SelectionSort().sort(arr);
+
+        //printing sorted array
+        Arrays.stream(arr).forEach(element -> System.out.print(element + "\t"));
+    }
 }
